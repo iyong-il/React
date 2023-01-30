39bfa6df27b3f997ada97c456fb49ddb3fc484d8
iyong-il <lyi1233313@gmail.com>
2023-01-30 22:36:40 +0900

    23.01.30 / 제목만들기

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,16 @@
-import logo from "./logo.svg";
+/* eslint-disable */
+
+// import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          안녕하세요.
-        </a>
-      </header>
+    <div className="container">
+      <div className="todoBlock">
+        <div className="title">
+          <h1>할 일 목록</h1>
+        </div>
+      </div>
     </div>
   );
 }
