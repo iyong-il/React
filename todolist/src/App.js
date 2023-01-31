3839d251946ae301ab2dd2d04b766350463c0150
iyong-il <lyi1233313@gmail.com>
2023-01-31 20:50:15 +0900

    23.01.31 / map() 사용

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -4,12 +4,20 @@
 import "./App.css";
 
 function App() {
+  let todoData = [{ id: "1", title: "sss", isComplete: false }];
   return (
     <div className="container">
       <div className="todoBlock">
         <div className="title">
           <h1>할 일 목록</h1>
         </div>
+        {todoData.map((e) => (
+          <div className="list" key={e.id}>
+            <input type="checkbox" />
+            {e.title}
+            <button className="deleteButton">X</button>
+          </div>
+        ))}
       </div>
     </div>
   );
